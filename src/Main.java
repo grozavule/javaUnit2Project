5d3624ea63599bc3fef1299d8d0a0785b4518693
Eric Drake <puturosule@gmail.com>
2023-01-20 00:07:50 -0700

    completed Part 3

---
+++ src/Main.java
@@ -0,0 +1,193 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        //create an ArrayList to hold the cupcake menu
+        ArrayList<Cupcake> cupcakeMenu = new ArrayList<>();
+
+        //create the cupcakes
+        Cupcake cupcake = new Cupcake("standard cupcake");
+        RedVelvetCupcake redVelvetCupcake = new RedVelvetCupcake("red velvet cupcake");
+        ChocolateCupcake chocolateCupcake = new ChocolateCupcake("chocolate cupcake");
+
+        double cupcakePrice = getValidPrice(cupcake);
+        cupcake.setPrice(cupcakePrice);
+
+        double redVelvetPrice = getValidPrice(redVelvetCupcake);
+        redVelvetCupcake.setPrice(redVelvetPrice);
+
+        double chocolatePrice = getValidPrice(chocolateCupcake);
+        chocolateCupcake.setPrice(chocolatePrice);
+
+        //add all the cupcakes to the menu
+        cupcakeMenu.add(cupcake);
+        cupcakeMenu.add(redVelvetCupcake);
+        cupcakeMenu.add(chocolateCupcake);
+
+        //create an ArrayList to hold the drink menu
+        ArrayList<Drink> drinkMenu = new ArrayList<>();
+
+        Drink water = new Drink("drink of water");
+        Soda soda = new Soda("bottle of soda");
+        Milk milk = new Milk("bottle of milk");
+
+        //get the price for each type of drink from the user
+        double waterPrice = getValidPrice(water);
+        water.setPrice(waterPrice);
+
+        double sodaPrice = getValidPrice(soda);
+        soda.setPrice(sodaPrice);
+
+        double milkPrice = getValidPrice(milk);
+        milk.setPrice(milkPrice);
+
+        drinkMenu.add(water);
+        drinkMenu.add(soda);
+        drinkMenu.add(milk);
+
+        new Order(cupcakeMenu, drinkMenu);
+
+        Main.input.close();
+    }
+
+    public static double getValidPrice(MenuItem item) {
+        while(true){
+            try {
+                System.out.println("We are deciding on the price for our " + item.getItemName()
+                        + ". Here is the description:");
+                item.type();
+                System.out.println("How much would you like to charge for the " + item.getItemName()
+                        + "? (Input a numerical value taken to 2 decimal places):");
+                String priceText = input.nextLine();
+                if(!priceText.contains(".")){
+                    throw new NumberFormatException("The price amount needs to be expressed as both dollars and cents.\n");
+                }
+                double price = Double.parseDouble(priceText);
+                if(price < 0){
+                    throw new NumberFormatException("Nice try, wise guy! Next time enter a POSITIVE dollar amount.\n");
+                }
+                return price;
+            } catch(NumberFormatException e){
+                System.out.println(e.getMessage() + "\n");
+                continue;
+            }
+        }
+    }
+}
+
+class MenuItem {
+    private double price;
+    private String itemName;
+
+    public MenuItem(){
+
+    }
+
+    public MenuItem(String itemName){
+        this.itemName = itemName;
+    }
+
+    public MenuItem(MenuItem menuItem){
+        this(menuItem.itemName);
+    }
+
+    public double getPrice(){
+        return this.price;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public String getItemName(){
+        return this.itemName;
+    }
+
+    public void setItemName(String itemName){
+        this.itemName = itemName;
+    }
+
+    public void type(){
+        System.out.println(this.itemName);
+    }
+}
+
+
+class Cupcake extends MenuItem {
+    public Cupcake(String itemName){
+        super(itemName);
+    }
+
+    public Cupcake(Cupcake cupcake){
+        super(cupcake);
+        super.setPrice(cupcake.getPrice());
+
+    }
+    public void type(){
+        System.out.println("A basic, generic cupcake, with vanilla frosting");
+    }
+}
+
+class RedVelvetCupcake extends Cupcake {
+
+    public RedVelvetCupcake(String itemName){
+        super(itemName);
+    }
+
+    public RedVelvetCupcake(RedVelvetCupcake redVelvetCupcake){
+        super(redVelvetCupcake);
+    }
+    public void type(){
+        System.out.println("A red-velvet-based cupcake with cream cheese frosting.");
+    }
+}
+
+class ChocolateCupcake extends Cupcake {
+
+    public ChocolateCupcake(String itemName){
+        super(itemName);
+    }
+
+    public ChocolateCupcake(ChocolateCupcake chocolateCupcake){
+        super(chocolateCupcake);
+    }
+    public void type(){
+        System.out.println("A chocolate-based cupcake with chocolate frosting.");
+    }
+}
+
+class Drink extends MenuItem {
+    public Drink(String itemName){
+        super(itemName);
+    }
+
+    public Drink(Drink drink){
+        super(drink);
+        super.setPrice(drink.getPrice());
+    }
+    public void type(){
+        System.out.println(super.getItemName());
+    }
+}
+
+class Soda extends Drink {
+    public Soda(String itemName){
+        super(itemName);
+    }
+
+    public Soda(Soda soda){
+        super(soda);
+    }
+}
+
+class Milk extends Drink {
+    public Milk(String shortDescription){
+        super(shortDescription);
+    }
+
+    public Milk(Milk milk){
+        super(milk);
+    }
+}
---
+++ src/Order.java
@@ -0,0 +1,102 @@
+import java.util.ArrayList;
+
+public class Order {
+
+    ArrayList<Cupcake> cupcakeMenu = new ArrayList<>();
+    ArrayList<Drink> drinkMenu = new ArrayList<>();
+    int menuItemCounter = 0;
+
+    public Order(ArrayList<Cupcake> cupcakeMenu, ArrayList<Drink> drinkMenu){
+        //make copies of the menu parameters
+        for(Cupcake cupcake : cupcakeMenu) {
+            this.cupcakeMenu.add(new Cupcake(cupcake));
+        }
+
+        for(Drink drink : drinkMenu){
+            this.drinkMenu.add(new Drink(drink));
+        }
+
+        //prompt the user to place an order
+        System.out.println("Would you like to place an order? (Y or N)");
+        String placeOrder = Main.input.nextLine();
+
+        ArrayList<MenuItem> order = new ArrayList<>();
+        if(placeOrder.equalsIgnoreCase("Y") || placeOrder.equalsIgnoreCase("YES")){
+            System.out.println("CUPCAKES:");
+            this.displayMenu(cupcakeMenu);
+
+            System.out.println("\nDRINKS:");
+            this.displayMenu(drinkMenu);
+
+            boolean ordering = true;
+            while(ordering){
+                System.out.println("What would you like to order? Please use the number associated with each item to order.");
+                int orderChoice = Main.input.nextInt();
+                Main.input.nextLine();
+
+                switch(orderChoice){
+                    case 0:
+                        order.add(new Cupcake(this.cupcakeMenu.get(orderChoice)));
+                        break;
+                    case 1:
+                        String redVelvetItemName = this.cupcakeMenu.get(orderChoice).getItemName();
+                        double redVelvetPrice = this.cupcakeMenu.get(orderChoice).getPrice();
+                        RedVelvetCupcake redVelvetCupcake = new RedVelvetCupcake(redVelvetItemName);
+                        redVelvetCupcake.setPrice(redVelvetPrice);
+                        order.add(redVelvetCupcake);
+                        break;
+                    case 2:
+                        String chocolateItemName = this.cupcakeMenu.get(orderChoice).getItemName();
+                        double chocolatePrice = this.cupcakeMenu.get(orderChoice).getPrice();
+                        ChocolateCupcake chocolateCupcake = new ChocolateCupcake(chocolateItemName);
+                        chocolateCupcake.setPrice(chocolatePrice);
+                        order.add(chocolateCupcake);
+                        break;
+                    case 3:
+                        order.add(new Drink(this.drinkMenu.get(0)));
+                        break;
+                    case 4:
+                        String sodaItemName = this.drinkMenu.get(1).getItemName();
+                        double sodaPrice = this.drinkMenu.get(1).getPrice();
+                        Soda soda = new Soda(sodaItemName);
+                        soda.setPrice(sodaPrice);
+                        order.add(soda);
+                        break;
+                    case 5:
+                        String milkItemName = this.drinkMenu.get(2).getItemName();
+                        double milkPrice = this.drinkMenu.get(2).getPrice();
+                        Milk milk = new Milk(milkItemName);
+                        milk.setPrice(milkPrice);
+                        order.add(milk);
+                        break;
+                    default:
+                        System.out.println("Sorry, we don't seem to have that on the menu.");
+                }
+                System.out.println("Would you like to continue ordering? (Y or N)");
+                placeOrder = Main.input.nextLine();
+                if(!placeOrder.equalsIgnoreCase("Y") && !placeOrder.equalsIgnoreCase("YES")){
+                    System.out.println("Your order total: " + this.calculateOrderTotal(order));
+                    System.out.println("Thank you for your business.");
+                    ordering = false;
+                }
+            }
+        } else {
+            System.out.println("Have a nice day then.");
+            System.exit(0);
+        }
+    }
+
+    private void displayMenu(ArrayList<? extends MenuItem> menu){
+        for(MenuItem item : menu){
+            System.out.println(this.menuItemCounter++ + "\t" + item.getItemName() + "\t\t" + item.getPrice());
+        }
+    }
+
+    private double calculateOrderTotal(ArrayList<? extends MenuItem> order){
+        double total = 0;
+        for(MenuItem item : order){
+            total += item.getPrice();
+        }
+        return total;
+    }
+}
